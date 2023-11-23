68ee4c8009dd26d169e98d0665b76ce474e5b3de
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-11-23 09:27:55 -0300

    Quase terminado

---
+++ assets/main.js
@@ -0,0 +1,26 @@
+const localStorageKey = 'to-do-list-gn';
+function newTask() {
+    let input = document.getElementById('input-new-task');
+    
+    if (!input.value) {
+        alert('Digite algo para inserir em sua lista');
+    }
+    else {
+        let values = JSON.parse(localStorage.getItem('to-do-list-gn') || "[]");
+        values.push({
+            name: input.value
+        })
+        localStorage.setItem(localStorageKey,JSON.stringify(values));
+        showValues();
+    }
+}
+
+function showValues() {
+    let values = JSON.parse(localStorage.getItem('to-do-list-gn') || "[]");
+    let list = document.getElementById('to-do-list');
+    list.innerHTML = '';
+    for (let i = 1; i <= values.length; i++) {
+        list.innerHTML += `<li>${values[i]['name']}</li>`;
+    }
+    
+}
