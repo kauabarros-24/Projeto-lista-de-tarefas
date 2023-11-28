b86f166c292b2f71dfa795c0e94750b3235e0c27
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-11-28 16:07:55 -0300

    Projeto quase finalizado

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,9 @@
 const localStorageKey = 'to-do-list-gn'
 
 function validateIfExistsNewTask() {
-    let values     = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
     let inputValue = document.getElementById('input-new-task').value;
-    let exists     = values.find(x => x.name == inputValue);
+    let exists = values.find(x => x.name == inputValue);
     return !exists ? false : true;
 }
 
@@ -11,20 +11,20 @@ function newTask() {
     let input = document.getElementById('input-new-task');
     input.style.border = '';
 
-    if(!input.value) {
+    if (!input.value) {
         input.style.border = '2px solid red';
         alert('Digite algo para inserir em sua lista');
     }
-    else if(validateIfExistsNewTask())  {
+    else if (validateIfExistsNewTask()) {
         alert('Já existe uma task com essa descrição');
     }
     else {
         let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
-        values.push( {
+        values.push({
             name: input.value
         })
-        localStorage.setItem(localStorageKey,JSON.stringify(values));
-        showValues();     
+        localStorage.setItem(localStorageKey, JSON.stringify(values));
+        showValues();
     }
     input.value = '';
 }
@@ -33,7 +33,7 @@ function showValues() {
     let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
     let list = document.getElementById('to-do-list');
     list.innerHTML = '';
-    for(let i = 0; i < values.length; i++) {
+    for (let i = 0; i < values.length; i++) {
         list.innerHTML += `<li>${values[i]['name']}<button id='btn-ok' onclick='removeItem("${values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/></svg></button></li>`;
     }
 }
@@ -41,8 +41,8 @@ function showValues() {
 function removeItem(data) {
     let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
     let index = values.findIndex(x => x.name == data);
-    values.splice(index,1);
-    localStorage.setItem(localStorageKey,JSON.stringify(values));
+    values.splice(index, 1);
+    localStorage.setItem(localStorageKey, JSON.stringify(values));
     showValues();
 }
 
